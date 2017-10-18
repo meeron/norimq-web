6ebd033b5ec3a18a548d3d734a10d0f86c0b9068
Unknown <mijakubowski@gmail.com>
2017-10-18 20:41:37 +0200

    Table of queues

--- src/Components/Queues.js
+++ src/Components/Queues.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Container } from 'bloomer'
+import { Container, Table } from 'bloomer'
 import Api from '../Services/apiService'
 
 class Queues extends Component {
@@ -17,15 +17,35 @@ class Queues extends Component {
       .catch(ex => console.error(ex));
   }
 
+  _renderRow = (q) => (
+    <tr key={q._id}>
+      <td><Link to={`/queues/${q.name}`}>{q.name}</Link></td>
+      <td><span>{q.size}</span></td>
+      <td><span>{q.messages_count}</span></td>
+      <td><span>{q.created_at}</span></td>
+      <td><span>{q.updated_at}</span></td>
+    </tr>
+  )
+
   render() {
     return (
       <Container>
-        <ul>
-          {
-            this.state.queues.map(q =>
-              <li><Link to={"/queues/1"}>{q.name}</Link></li>)
-          }
-        </ul>
+        <Table isBordered isNarrow>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Size</th>
+              <th>Messages</th>
+              <th>Created</th>
+              <th>Updated</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.queues.map(q => this._renderRow(q))
+            }
+          </tbody>
+        </Table>
       </Container>
     )
   }
